3262313cc0d9472f1688a09b5b6dbd3055b3468a
tobinbradley <tobin.bradley@gmail.com>
2020-01-28 16:53:19 -0500

    postgis 3 improvements, add precision option

--- routes/geojson.js
+++ routes/geojson.js
@@ -1,56 +1,39 @@
-const sm = require('@mapbox/sphericalmercator')
-const merc = new sm({
-  size: 256
-});
-
 // route query
 const sql = (params, query) => {
   let bounds = query.bounds ? query.bounds.split(',').map(Number) : null;
-  bounds && bounds.length === 3 ? bounds = merc.bbox(bounds[1], bounds[2], bounds[0]) : null;
-  
+
   return `
-  SELECT 
-    Row_to_json(fc) as geojson
-
-  FROM (
-    SELECT 
-      'FeatureCollection' AS type, 
-      COALESCE(Array_to_json(Array_agg(f)), '[]'::json) AS features
-
-  FROM (
-    SELECT 
-      'Feature' AS type, 
-      St_asgeojson(ST_Transform(lg.${query.geom_column}, 4326))::json AS geometry,
-      ${query.columns ? ` 
-      Row_to_json(
-        (
-          SELECT 
-            l 
-          FROM   
-         (SELECT ${query.columns}) AS l
-        )
-      ) AS properties 
-      ` : `'{}'::json AS properties`}
-                
-    FROM   
-      ${params.table} AS lg
-      ${bounds ? `, (SELECT ST_SRID(${query.geom_column}) as srid FROM ${params.table} LIMIT 1) sq` : ''}
-      
-    
-    -- Optional Filter
-    ${query.filter || bounds ? 'WHERE' : ''}
-    ${query.filter ? `${query.filter}` : '' }
-    ${query.filter && bounds ? 'AND' : ''}
-    ${bounds ? `      
-      ${query.geom_column} &&
-      ST_Transform(
-        ST_MakeEnvelope(${bounds.join()}, 4326), 
-        srid
-      )      
-    ` : ''}
-
-    ) AS f
-  ) AS fc; 
+    SELECT
+      ST_AsGeoJSON(subq.*, '', ${parseInt(query.precision, 10)}) AS geojson
+    FROM (
+      SELECT
+        ST_Transform(${query.geom_column}, 4326) as geom
+        ${query.columns ? `, ${query.columns}` : ''}
+      FROM
+        ${params.table},
+        (SELECT ST_SRID(${query.geom_column}) AS srid FROM ${params.table} LIMIT 1) a
+      ${query.filter || bounds ? 'WHERE' : ''}
+        ${query.filter ? `${query.filter}` : ''}
+        ${query.filter && bounds ? 'AND' : ''}
+        ${bounds && bounds.length === 4 ?
+          `${query.geom_column} &&
+          ST_Transform(
+            ST_MakeEnvelope(${bounds.join()}, 4326),
+            srid
+          )
+          `
+          : ''
+        }
+        ${bounds && bounds.length === 3 ?
+          `${query.geom_column} &&
+          ST_Transform(
+            ST_TileEnvelope(${bounds.join()}),
+            srid
+          )
+          `
+          : ''
+        }
+    ) as subq
   `
 }
 
@@ -83,6 +66,11 @@ const schema = {
       type: 'string',
       pattern: '^-?[0-9]{0,20}.?[0-9]{1,20}?(,-?[0-9]{0,20}.?[0-9]{1,20}?){2,3}$',
       description: 'Optionally limit output to features that intersect bounding box. Can be expressed as a bounding box (sw.lng, sw.lat, ne.lng, ne.lat) or a Z/X/Y tile (0,0,0).'
+    },
+    precision: {
+      type: 'integer',
+      description: 'The maximum number of decimal places to return. Default is 9.',
+      default: 9
     }
   }
 }
@@ -106,8 +94,18 @@ module.exports = function (fastify, opts, next) {
         client.query(
           sql(request.params, request.query),
           function onResult(err, result) {
-            release()
-            reply.send(err || result.rows[0].geojson)
+            if (err) {
+              reply.send(err)
+            } else {
+              if (!result.rows[0].geojson) {
+                reply.code(204)
+              }
+              const json = {
+                type: 'FeatureCollection',
+                features: result.rows.map(el => JSON.parse(el.geojson))
+              }
+              reply.send(json)
+            }
           }
         )
       }
